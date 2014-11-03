c21b95004ac348482cd1bc4d906ee8a78f62bf0b
Luke Carrier <luke@carrier.im>
2014-11-03 21:43:23 +0000

    All the usual boilerplate

---
+++ README.md
@@ -0,0 +1,16 @@
+Moodle Developer Kit
+====================
+
+Moodle provides a pretty great framework that empowers developers with most of
+the tools they need to create awesome utilities for educators. This Moodle
+plugin helps to smooth over some of the current cracks in these foundations,
+simplifying development and helping you get on with your projects.
+
+Features
+--------
+
+* A configuration assistance library
+    * Per git branch configuration of development sites
+    * Automatic configuration of wwwroot based upon
+      ```$_SERVER['SERVER_ADDR']```
+* Shortcuts for plugin installation and configuration
---
+++ src/lang/en/local_devkit.php
@@ -0,0 +1,30 @@
+<?php
+
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle. If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Moodle/Totara LMS DevKit plugin.
+ *
+ * A suite of developer tools that aim to smooth over some of the cracks in
+ * Moodle. This plugin should absolutely not be installed on production sites.
+ *
+ * @author Luke Carrier <luke@carrier.im>
+ * @copyright 2014 Luke Carrier
+ * @license GPL v3
+ */
+
+// Module metadata
+$string['pluginname'] = 'DevKit';
