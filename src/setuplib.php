f006246ef38fb9eeb654673bae5cbe44e5f6e2c4
Luke Carrier <luke@carrier.im>
2014-12-04 13:44:00 +0000

    Support Totara LMS series detection

--- src/setuplib.php
+++ src/setuplib.php
@@ -26,11 +26,59 @@
  * @license GPL v3
  */
 
+/**
+ * Early setup exception.
+ *
+ * Raised when the devkit module is unable to retrieve values used for early
+ * initialisation of the Moodle site.
+ */
+class local_devkit_setup_exception extends Exception {
+    /**
+     * Initialiser.
+     *
+     * @param string     $message  Error message.
+     * @param integer    $code     Error code.
+     * @param \Exception $previous Previous exception (if raised during the
+     *                             handling of another exception).
+     */
+    public function __construct($message, $code=0, Exception $previous=null) {
+        parent::__construct($message, $code, $exception);
+    }
+
+    /**
+     * Return a string representation of the error condition.
+     *
+     * @return string
+     */
+    public function __toString() {
+        return __CLASS__ . ": [{$this->code}]: {$this->message}";
+    }
+}
+
 /* Don't check for MOODLE_INTERNAL here, as this library is designed to be
 /* sourced prior to /lib/setup.php. */
 
 if (!defined('LOCAL_DEVKIT_SETUPLIB')) {
+    /**
+     * Has devkit initialised?
+     *
+     * @var boolean
+     */
     define('LOCAL_DEVKIT_SETUPLIB', true);
+
+    /**
+     * Distribution: Moodle.
+     *
+     * @var integer
+     */
+    define('LOCAL_DEVKIT_DISTRO_MOODLE', 1);
+
+    /**
+     * Distribution: Moodle.
+     *
+     * @var integer
+     */
+    define('LOCAL_DEVKIT_DISTRO_TOTARALMS', 2);
 
     /**
      * Determine the current Moodle series from Git's HEAD.
@@ -51,15 +99,45 @@ if (!defined('LOCAL_DEVKIT_SETUPLIB')) {
      *                          being configured!
      *
      * @return string Either a (two digit) number extracted from a
-     *                MOODLE_xx_STABLE branch name, or "master".
+     *                MOODLE_xx_STABLE or tx-release-x.x branch name, or simply
+     *                "master".
      */
-    function local_devkit_moodle_series($directory) {
+    function local_devkit_moodle_series($directory, $distro=null) {
+        if ($distro === null) {
+            $distro = LOCAL_DEVKIT_DISTRO_MOODLE;
+        }
+
         $headcontents = file_get_contents("{$directory}/.git/HEAD");
 
-        preg_match('/MOODLE_([0-9]+)_STABLE$/', $headcontents, $seriesmatches);
-        preg_match('/([a-zA-Z0-9]+)$/',         $headcontents, $rawmatches);
+        switch ($distro) {
+            case LOCAL_DEVKIT_DISTRO_MOODLE:
+                preg_match('/MOODLE_([0-9]+)_STABLE$/', $headcontents, $seriesmatches);
+                preg_match('/([a-zA-Z0-9]+)$/',         $headcontents, $rawmatches);
 
-        return (count($seriesmatches) === 2) ? $seriesmatches[1] : $rawmatches[1];
+                $series = (count($seriesmatches) === 2)
+                        ? $seriesmatches[1] : $rawmatches[1];
+
+                break;
+
+            case LOCAL_DEVKIT_DISTRO_TOTARALMS:
+                preg_match('/t[0-9]-release-([0-9]+)\.([0-9]+)/', $headcontents, $seriesmatches);
+                preg_match('/([a-zA-Z0-9]+)$/',                   $headcontents, $rawmatches);
+
+                $series = (count($seriesmatches) === 3)
+                        ? "{$seriesmatches[1]}{$seriesmatches[2]}"
+                        : $rawmatches[1];
+
+                break;
+
+            default:
+                throw new local_devkit_setup_exception('invalid distribution; please supply a LOCAL_DEVKIT_DISTRO_* constant value');
+        }
+
+        if (!isset($series)) {
+            throw new local_devkit_setup_exception('failed to determine the series from the branch name');
+        }
+
+        return $series;
     }
 
     /**
